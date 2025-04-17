e3b431f54c2b8356edfec4550111886ced939758
Santiago Montoya <sanmonsua@gmail.com>
2025-04-17 18:19:08 -0500

    implement reverse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+
+fn reverse(s: &String) -> String {
+    let chars = s.chars().rev();
+    let reversed_string = String::from_iter(chars);
+    reversed_string
+}
+
 fn main() {
-    println!("Hello, world!");
+    let my_input = String::from("Hello Santi");
+    let reversed = reverse(&my_input);
+    println!("my_input reversed is {reversed}");
 }
