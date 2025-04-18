c53069ee2ff31450917f2e1c7e6193578f4fb5d0
Santiago Montoya <sanmonsua@gmail.com>
2025-04-17 20:20:14 -0500

    add words and chars uniqueness checks

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 
-fn reverse(s: &String) -> String {
+fn reverse(s: &str) -> String {
     let chars = s.chars().rev();
     let reversed_string = String::from_iter(chars);
     reversed_string
 }
 
-fn is_palindrome(s: &String) -> bool {
+fn is_palindrome(s: &str) -> bool {
     let reversed = reverse(s);
     reversed == *s
 }
 
-fn count_chars(s: &String) -> usize {
+fn count_chars(s: &str) -> usize {
     s.chars().count()
 }
 
-fn split_words(s: &String) -> Vec<&str> {
+fn split_words(s: &str) -> Vec<&str> {
     if count_chars(s) == 0 {
         let words: Vec<&str> = Vec::new();
         return words
@@ -26,33 +26,69 @@ fn split_words(s: &String) -> Vec<&str> {
     for (i, c) in s.char_indices() {
         if c == ' ' && !is_prev_space {
             words.push(&s[last_pos..i]);
-            last_pos = i;
+            last_pos = i+1;
             is_prev_space = true;
         } else {
             is_prev_space = c == ' ';
         }
     }
 
-    if !is_prev_space {
+    if !is_prev_space && last_pos < s.chars().count() {
         words.push(&s[last_pos..]);
     }
 
     words
 }
 
-fn count_words(s: &String) -> usize {
+fn count_words(s: &str) -> usize {
     let words = split_words(s);
     words.len()
 }
 
+fn unique_chars(s: &str) -> bool {
+    let mut seen = std::collections::HashSet::new();
+    for c in s.chars() {
+        if !seen.insert(c) {
+            return false;
+        }
+    }
+    true
+}
+
+fn unique_words(s: &str) -> bool {
+    let mut seen = std::collections::HashSet::new();
+    let words = split_words(s);
+    for w in words {
+        if !seen.insert(w) {
+            return false;
+        }
+    }
+    true
+}
+
 fn main() {
-    let my_input = String::from("kayak  and lust");
+    let my_input = String::from("abc abc");
     let reversed = reverse(&my_input);
+    print!("my_input reversed is {reversed}");
+
     let palindrome = is_palindrome(&my_input);
-    let char_count = count_chars(&my_input);
-    let word_count = count_words(&my_input);
     match palindrome {
-        true => println!("my_input reversed is {reversed}, and is a palindrome. Len: {char_count}. Words: {word_count}"),
-        false => println!("my_input reversed is {reversed}, and is not a palindrome. Len: {char_count}. Words: {word_count}"),
+        true => println!("is a palindrome."),
+        false => println!("is not a palindrome"),
+    }
+
+    let char_count = count_chars(&my_input);
+    let has_unique_chars = unique_chars(&my_input);
+
+    match has_unique_chars {
+        true => println!("Char count: {char_count} (All Unique)"),
+        false => println!("Char count: {char_count} (Not Unique)"),
+    }
+
+    let word_count = count_words(&my_input);
+    let has_unique_words = unique_words(&my_input);
+    match has_unique_words {
+        true => println!("Word count: {word_count} (All Unique)"),
+        false => println!("Word count: {word_count} (Not Unique)"),
     }
 }
